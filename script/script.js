3a0414f9d3f9d0860fa4921f06c6fc8c19daff57
arifalhidayah <78639582+arifalhidayah@users.noreply.github.com>
2023-10-22 14:42:30 +0700

    images slider
    add DOM Javascript to make images slider

---
+++ script/script.js
@@ -0,0 +1,27 @@
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const maxScollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    // Slide image according to the slide button clicks
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
+    });
+
+    // Hide the slide button if there is no image again
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "block";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScollLeft ? "none" : "block";        
+    }
+
+    imageList.addEventListener("scroll", () => {
+        handleSlideButtons();
+    });
+}
+
+
+window.addEventListener("load", initSlider);
